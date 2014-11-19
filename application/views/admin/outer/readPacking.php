566dee6b2f40dd40257deae7f05a460b4997309c
KPBAEK <KPBAEK@sbmkorea.com>
2014-11-19 17:11:42 +0900

    Change-Id: I86ab7ca888004150360d993b616d5926f9e12f5d

---
+++ application/views/admin/outer/readPacking.php
@@ -0,0 +1,98 @@
+<?php
+function getPackingMailCtnt($ctnt, $invoice){
+
+	$ctnt = str_replace("@txt_invoice_dt", $invoice['invoiceInfo']['txt_invoice_dt'], $ctnt);
+	$ctnt = str_replace("@txt_pi_no", $invoice['invoiceInfo']['pi_no']. "-" . $invoice['invoiceInfo']['pi_sndmail_seq'], $ctnt);
+	$ctnt = str_replace("@csn_cmpy_nm", $invoice['invoiceInfo']['csn_cmpy_nm'], $ctnt);
+	$ctnt = str_replace("@csn_attn", $invoice['invoiceInfo']['csn_attn'], $ctnt);
+	$ctnt = str_replace("@destnt", $invoice['invoiceInfo']['destnt'], $ctnt);
+	$ctnt = str_replace("@cntry", $invoice['invoiceInfo']['cntry'], $ctnt);
+	$ctnt = str_replace("@txt_ship_port_atcd", $invoice['invoiceInfo']['txt_ship_port_atcd'], $ctnt);
+	$ctnt = str_replace("@inv_payment", $invoice['invoiceInfo']['inv_payment'], $ctnt);
+	
+	if($invoice['invoiceInfo']['ci_sndmail_seq']!=null){
+		$ctnt = str_replace("@txt_invoice_no", $invoice['invoiceInfo']['pi_no']. "-" . $invoice['invoiceInfo']['ci_sndmail_seq'], $ctnt);
+	}else{
+		$ctnt = str_replace("@txt_invoice_no", $invoice['invoiceInfo']['pi_no'], $ctnt);
+	}
+	
+	if($invoice['invoiceInfo']['buyer']!=$invoice['invoiceInfo']['csn_attn']){
+		$ctnt = str_replace("@buyer", $invoice['invoiceInfo']['buyer'], $ctnt);
+	}else{
+		$ctnt = str_replace("@buyer", "SAME AS CONSIGNEE", $ctnt);
+	}
+	
+	
+	$ctnt = str_replace("@csn_addr", $invoice['invoiceInfo']['csn_addr'], $ctnt);
+	$ctnt = str_replace("@csn_tel", $invoice['invoiceInfo']['csn_tel'], $ctnt);
+	$ctnt = str_replace("@csn_fax", $invoice['invoiceInfo']['csn_fax'], $ctnt);
+	$ctnt = str_replace("@csn_attn", $invoice['invoiceInfo']['csn_attn'], $ctnt);
+	
+	
+	$tot_qty = 0;
+	if($invoice['orderEqpList']==null){
+		$ctnt = str_replace("@eqpDiv", "none", $ctnt);
+	}else{
+		$ctnt = str_replace("@eqpDiv", "", $ctnt);
+	
+		$mdl_nm = "";
+		$eqp_qty = "";
+		$eqp_net_wgt = "";
+		foreach($invoice['orderEqpList'] as $orderEqpList) {
+			$mdl_nm .= $orderEqpList['mdl_nm'] . "  Currency Discrimination Counter ( " . $orderEqpList['eqp_qty'] . " )<br>";
+			$eqp_qty += $orderEqpList['eqp_qty'];
+			$tot_qty += $orderEqpList['eqp_qty'];
+			$eqp_net_wgt += $orderEqpList['net_wgt'];
+		}
+		$ctnt = str_replace("@mdl_nm", $mdl_nm, $ctnt);
+		$ctnt = str_replace("@eqp_cartons", $eqp_qty, $ctnt);
+		$ctnt = str_replace("@eqp_net_wgt", $eqp_net_wgt, $ctnt);
+	}
+	
+	if($invoice['orderPartList']==null){
+		$ctnt = str_replace("@spareDiv", "none", $ctnt);
+	}else{
+		$ctnt = str_replace("@spareDiv", "", $ctnt);
+		$part_net_wgt = "";
+		foreach($invoice['orderPartList'] as $orderPartList) {
+			$part_net_wgt += $orderPartList['net_wgt'];
+		}
+		$ctnt = str_replace("@part_net_wgt", $part_net_wgt, $ctnt);
+	}
+	
+	
+	$ctnt = str_replace("@eqp_carton_no", $invoice['packingInfo']['eqp_carton_no'], $ctnt);
+	$ctnt = str_replace("@part_carton_no", $invoice['packingInfo']['part_carton_no'], $ctnt);
+	$ctnt = str_replace("@addon_carton_no", $invoice['packingInfo']['addon_carton_no'], $ctnt);
+	$ctnt = str_replace("@part_cartons", $invoice['packingInfo']['part_cartons'], $ctnt);
+	$ctnt = str_replace("@addon_cartons", $invoice['packingInfo']['addon_cartons'], $ctnt);
+	$ctnt = str_replace("@eqp_gross_wgt", $invoice['packingInfo']['eqp_gross_wgt'], $ctnt);
+	$ctnt = str_replace("@part_gross_wgt", $invoice['packingInfo']['part_gross_wgt'], $ctnt);
+	$ctnt = str_replace("@addon_gross_wgt", $invoice['packingInfo']['addon_gross_wgt'], $ctnt);
+	$ctnt = str_replace("@tot_cartons", $invoice['packingInfo']['tot_cartons'], $ctnt);
+	$ctnt = str_replace("@tot_gross_wgt", $invoice['packingInfo']['tot_gross_wgt'], $ctnt);
+	$ctnt = str_replace("@sndmail_seq", $invoice['packingInfo']['sndmail_seq'], $ctnt);
+	$ctnt = str_replace("@udt_dt", $invoice['packingInfo']['udt_dt'], $ctnt);
+
+	
+	if($invoice['invoiceInfo']['prn_qty']!=null || $invoice['invoiceInfo']['repr_qty']!=null){
+		$ctnt = str_replace("@addonDiv", "", $ctnt);
+		$addon = "";
+		if($invoice['invoiceInfo']['prn_qty']!=null){
+			$addon .= "Printer";
+		}
+		if($invoice['invoiceInfo']['prn_qty']!=null && $invoice['invoiceInfo']['repr_qty']!=null){
+			$addon .= ", ";
+		}
+		if($invoice['invoiceInfo']['repr_qty']!=null){
+			$addon .= "Repair Parts";
+		}
+		$ctnt = str_replace("@addon", $addon, $ctnt);
+	}else{
+		$ctnt = str_replace("@addonDiv", "none", $ctnt);
+	}
+	
+	return $ctnt;
+}
+
+?>
---
+++ application/views/admin/outer/savePacking.php
@@ -0,0 +1,134 @@
+<?php
+$pi_no = $_POST["pi_no"];
+
+$eqp_carton_no =  "";
+if(isset($_POST["eqp_carton_no"])){
+	$eqp_carton_no = trim($_POST["eqp_carton_no"]);
+}
+
+$part_carton_no =  "";
+if(isset($_POST["part_carton_no"])){
+	$part_carton_no = trim($_POST["part_carton_no"]);
+}
+
+$addon_carton_no =  "";
+if(isset($_POST["addon_carton_no"])){
+	$addon_carton_no = trim($_POST["addon_carton_no"]);
+}
+
+$part_cartons = null;
+if(isset($_POST["part_cartons"])){
+	$part_cartons = trim($_POST["part_cartons"]);
+}
+
+$addon_cartons = null;
+if(isset($_POST["addon_cartons"])){
+	$addon_cartons = trim($_POST["addon_cartons"]);
+}
+
+$eqp_gross_wgt = null;
+if(isset($_POST["eqp_gross_wgt"])){
+	$eqp_gross_wgt = trim($_POST["eqp_gross_wgt"]);
+}
+
+$part_gross_wgt = null;
+if(isset($_POST["part_gross_wgt"])){
+	$part_gross_wgt = trim($_POST["part_gross_wgt"]);
+}
+
+$addon_gross_wgt = null;
+if(isset($_POST["addon_gross_wgt"])){
+	$addon_gross_wgt = trim($_POST["addon_gross_wgt"]);
+}
+
+$tot_cartons = null;
+if(isset($_POST["tot_cartons"])){
+	$tot_cartons = trim($_POST["tot_cartons"]);
+}
+
+$tot_gross_wgt = null;
+if(isset($_POST["tot_gross_wgt"])){
+	$tot_gross_wgt = trim($_POST["tot_gross_wgt"]);
+}
+
+
+
+$tot_eqp_qty = null;
+if(isset($_POST["tot_eqp_qty"])){
+	$tot_eqp_qty = trim($_POST["tot_eqp_qty"]);
+}
+
+
+session_start();
+
+if(isSet($_POST['pi_no'])){
+	
+	$pi_no = mysql_real_escape_string($pi_no);
+	$eqp_carton_no = mysql_real_escape_string($eqp_carton_no);
+	$part_carton_no = mysql_real_escape_string($part_carton_no);
+	$addon_carton_no = mysql_real_escape_string($addon_carton_no);
+	
+	$sql = "SELECT (select atcd_nm from cm_cd_attr where cd = '0070' and atcd = a.wrk_tp_atcd) txt_wrk_tp_atcd FROM om_ord_inf";
+	$sql = $sql . " WHERE pi_no ='" .$pi_no. "'";
+	$sql = $sql . " AND wrk_tp_atcd = '00700610'";  // 00700610(Packing List 발송)
+#	echo $sql;
+	
+	$result=mysql_query($sql);
+	
+	if($result!=null){
+		$txt_wrk_tp_atcd = mysql_result($result,0,"txt_wrk_tp_atcd");
+		$qryInfo['qryInfo']['todo'] = "N";
+		$qryInfo['qryInfo']['txt_wrk_tp_atcd'] = $txt_wrk_tp_atcd;
+		
+	}else{
+		$qryInfo['qryInfo']['todo'] = "Y";
+		
+		$tot_cartons = $tot_eqp_qty + $part_cartons + $addon_cartons;
+		$tot_gross_wgt = $eqp_gross_wgt + $part_gross_wgt + $addon_gross_wgt;
+		
+		if($tot_eqp_qty==null){
+			$tot_eqp_qty = "null";
+		}
+		if($part_cartons==null){
+			$part_cartons = "null";
+		}
+		if($addon_cartons==null){
+			$addon_cartons = "null";
+		}
+		if($eqp_gross_wgt==null){
+			$eqp_gross_wgt = "null";
+		}
+		if($part_gross_wgt==null){
+			$part_gross_wgt = "null";
+		}
+		if($addon_gross_wgt==null){
+			$addon_gross_wgt = "null";
+		}
+
+		$sql_packing = "INSERT INTO om_packing";
+		$sql_packing = $sql_packing . " (pi_no, eqp_carton_no, part_carton_no, addon_carton_no, part_cartons, addon_cartons, eqp_gross_wgt, part_gross_wgt, addon_gross_wgt, tot_cartons, tot_gross_wgt, crt_dt, crt_uid)";
+		$sql_packing = $sql_packing . " VALUES ('" .$pi_no. "', '" .$eqp_carton_no. "', '" .$part_carton_no. "', '" .$addon_carton_no. "', " .$part_cartons. ", " .$addon_cartons. ", " .$eqp_gross_wgt. ", " .$part_gross_wgt. ", " .$addon_gross_wgt. ", " .$tot_cartons. ", " .$tot_gross_wgt. ", now(), '" .$_SESSION['ss_user']['uid']. "')";
+		$sql_packing = $sql_packing . " ON DUPLICATE KEY";
+		$sql_packing = $sql_packing . " UPDATE";
+		$sql_packing = $sql_packing . "   eqp_carton_no = '" .$eqp_carton_no. "'";
+		$sql_packing = $sql_packing . " , part_carton_no = '" .$part_carton_no. "'";
+		$sql_packing = $sql_packing . " , addon_carton_no = '" .$addon_carton_no. "'";
+		$sql_packing = $sql_packing . " , part_cartons = " .$part_cartons;
+		$sql_packing = $sql_packing . " , addon_cartons = " .$addon_cartons;
+		$sql_packing = $sql_packing . " , eqp_gross_wgt = " .$eqp_gross_wgt;
+		$sql_packing = $sql_packing . " , part_gross_wgt = " .$part_gross_wgt;
+		$sql_packing = $sql_packing . " , addon_gross_wgt = " .$addon_gross_wgt;
+		$sql_packing = $sql_packing . " , tot_cartons = " .$tot_cartons;
+		$sql_packing = $sql_packing . " , tot_gross_wgt = " .$tot_gross_wgt;
+		$sql_packing = $sql_packing . " , udt_uid = '" .$_SESSION['ss_user']['uid']. "'";
+						
+		#echo $sql_packing;
+		$result2 = mysql_query($sql_packing);
+		$qryInfo['qryInfo']['sql2'] = $sql_packing;
+		$qryInfo['qryInfo']['result2'] = $result2;
+		
+	}
+	echo json_encode($qryInfo);
+	
+}
+?>
