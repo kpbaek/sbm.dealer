9e25fd83520ece3c2437b998ff28eb3b74933b6c
KPBAEK <KPBAEK@sbmkorea.com>
2014-11-26 19:10:04 +0900

    Change-Id: I046a0988c80f94611f480ba41bc19dbe2a6edcdd

--- config.php
+++ config.php
@@ -1,10 +1,24 @@
 <?php
-// PHP Grid database connection settings
+// PHPGRID database connection settings
 define("PHPGRID_DBTYPE","mysql"); // or mysqli
 define("PHPGRID_DBHOST","localhost");
 define("PHPGRID_DBUSER","sbmkorea");
 define("PHPGRID_DBPASS","sbmkoreacom123");
 define("PHPGRID_DBNAME","test");
+
+// Local database connection settings
+define("SBMBIZ_DBTYPE","mysql"); // or mysqli
+define("SBMBIZ_DBHOST","localhost");
+define("SBMBIZ_DBUSER","sbmkorea");
+define("SBMBIZ_DBPASS","sbmkoreacom123");
+define("SBMBIZ_DBNAME","test");
+
+// sbmbiz database connection settings
+#define("SBMBIZ_DBTYPE","mysql"); // or mysqli
+#define("SBMBIZ_DBHOST","mysql.hostinger.kr");
+#define("SBMBIZ_DBUSER","u900714269_sbm");
+#define("SBMBIZ_DBPASS","sbmdealer!");
+#define("SBMBIZ_DBNAME","u900714269_sbm");
 
 // Automatically make db connection inside lib
 define("PHPGRID_AUTOCONNECT",0);
@@ -12,3 +26,19 @@ define("PHPGRID_AUTOCONNECT",0);
 // Basepath for lib
 #define("PHPGRID_LIBPATH",dirname(__FILE__).DIRECTORY_SEPARATOR."lib".DIRECTORY_SEPARATOR);
 define("PHPGRID_LIBPATH",$_SERVER["DOCUMENT_ROOT"]."/lib/");
+
+define('SBM_SMTP_HOST', 'mx1.hostinger.kr');
+define('SBM_SMTP_USER', 'sbmkorea@sbmkorea.url.ph');
+define('SBM_SMTP_PASS', 'sbmkoreacom');
+define('SBM_SMTP_PORT', '2525');
+
+define('SBM_PUB_EMAIL', 'sbm@sbmkorea.url.ph');
+define('SBM_LOCAL_EMAIL', 'kpbaek@localhost');
+
+define('LOCAL_SMTP_HOST', 'localhost');
+define('LOCAL_SMTP_USER', 'kpbaek');
+define('LOCAL_SMTP_PASS', '1111');
+define('LOCAL_SMTP_PORT', '25');
+
+define('SBM_DOMAIN', 'http://www.sbmkorea.url.ph');
+
