ccf97af3bbd151cc72ce5009e8477b38725fd4cd
KPBAEK <KPBAEK@sbmkorea.com>
2014-12-01 09:20:54 +0900

    Change-Id: I71f67d8475e203dfb19d1abb2704efed172492c3

--- config.php
+++ config.php
@@ -7,11 +7,11 @@ define("PHPGRID_DBPASS","sbmkoreacom123");
 define("PHPGRID_DBNAME","test");
 
 // Local database connection settings
-#define("SBMBIZ_DBTYPE","mysql"); // or mysqli
-#define("SBMBIZ_DBHOST","localhost");
-#define("SBMBIZ_DBUSER","sbmkorea");
-#define("SBMBIZ_DBPASS","sbmkoreacom123");
-#define("SBMBIZ_DBNAME","test");
+define("SBMBIZ_DBTYPE","mysql"); // or mysqli
+define("SBMBIZ_DBHOST","localhost");
+define("SBMBIZ_DBUSER","sbmkorea");
+define("SBMBIZ_DBPASS","sbmkoreacom123");
+define("SBMBIZ_DBNAME","test");
 
 // sbmbiz database connection settings
 #define("SBMBIZ_DBTYPE","mysql"); // or mysqli
@@ -21,11 +21,11 @@ define("PHPGRID_DBNAME","test");
 #define("SBMBIZ_DBNAME","u900714269_sbm");
 
 // sbmbiz database connection settings
-define("SBMBIZ_DBTYPE","mysql"); // or mysqli
-define("SBMBIZ_DBHOST","db.sbmkorea.biz");
-define("SBMBIZ_DBUSER","sbmbiz");
-define("SBMBIZ_DBPASS","dealer123!");
-define("SBMBIZ_DBNAME","dbsbmbiz");
+#define("SBMBIZ_DBTYPE","mysql"); // or mysqli
+#define("SBMBIZ_DBHOST","db.sbmkorea.biz");
+#define("SBMBIZ_DBUSER","sbmbiz");
+#define("SBMBIZ_DBPASS","dealer123!");
+#define("SBMBIZ_DBNAME","dbsbmbiz");
 
 // Automatically make db connection inside lib
 define("PHPGRID_AUTOCONNECT",0);
@@ -34,18 +34,18 @@ define("PHPGRID_AUTOCONNECT",0);
 #define("PHPGRID_LIBPATH",dirname(__FILE__).DIRECTORY_SEPARATOR."lib".DIRECTORY_SEPARATOR);
 define("PHPGRID_LIBPATH",$_SERVER["DOCUMENT_ROOT"]."/lib/");
 
-#define('SBM_SMTP_HOST', 'mx1.hostinger.kr');
+#define('SBM_SMTP_HOST', 'mail.sbmkorea.url.ph');
 #define('SBM_SMTP_USER', 'sbmkorea@sbmkorea.url.ph');
 #define('SBM_SMTP_PASS', 'sbmkoreacom');
 #define('SBM_SMTP_PORT', '2525');
 
-define('SBM_SMTP_HOST', 'mail.sbmkorea.biz');
-define('SBM_SMTP_USER', 'sbm@sbmkorea.biz');
-define('SBM_SMTP_PASS', 'sbmmail123');
-define('SBM_SMTP_PORT', '587');
+#define('SBM_SMTP_HOST', 'mail.sbmkorea.biz');
+#define('SBM_SMTP_USER', 'sbm@sbmkorea.biz');
+#define('SBM_SMTP_PASS', 'sbmmail123');
+#define('SBM_SMTP_PORT', '587');
 
 #define('SBM_PUB_EMAIL', 'sbm@sbmkorea.url.ph');
-define('SBM_PUB_EMAIL', 'sbm@sbmkorea.biz');
+#define('SBM_PUB_EMAIL', 'sbm@sbmkorea.biz');
 define('SBM_LOCAL_EMAIL', 'kpbaek@localhost');
 
 define('LOCAL_SMTP_HOST', 'localhost');
