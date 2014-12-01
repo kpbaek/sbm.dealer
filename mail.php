929faab9bc3121eaf1a15d5c9476326baea3e6a2
KPBAEK <KPBAEK@sbmkorea.com>
2014-12-01 10:26:56 +0900

    Change-Id: I11840220ea2a275d957737993ea72e21c4e7dab6

--- mail.php
+++ mail.php
@@ -7,23 +7,38 @@ $mail = new PHPMailer ();
 $mail->IsSMTP (); 
 $mail->SMTPDebug = 1;
 $mail->Mailer = "smtp";
-#$mail->Host = "mx1.hostinger.kr"; 
-$mail->Host = "mail.sbmkorea.biz"; 
 $mail->SMTPAuth = true; 
 
-#$mail->Port = "2525"; 
-#$mail->Username = "sbmkorea@sbmkorea.url.ph";
-#$mail->Password = "sbmkoreacom";
-#$mail->SetFrom ( 'sbmkorea@sbmkorea.url.ph' ); 
+//local
+/**
+$mail->Host = "localhost"; 
+$mail->Port = "25"; 
+$mail->Username = "kpbaek";
+$mail->Password = "1111";
+$mail->SetFrom ( 'kpbaek@localhost' ); 
+$mail->AddAddress ( "tester1@localhost", "SBM" );
+*/
 
+//sbmkorea.url.ph
+/**
+$mail->Host = "mail.sbmkorea.url.ph"; 
+$mail->Port = "2525"; 
+$mail->Username = "admin@sbmkorea.url.ph";
+$mail->Password = "sbm123";
+$mail->SetFrom ( 'admin@sbmkorea.url.ph' ); 
+$mail->Priority = 1;
+$mail->AddAddress ( "sbm@sbmkorea.url.ph", "SBM" );
+*/
+
+//sbmkorea.biz
+$mail->Host = "mail.sbmkorea.biz"; 
 $mail->Port = "587"; 
 $mail->Username = "sbm@sbmkorea.biz";
 $mail->Password = "sbmmail123";
 $mail->SetFrom ( 'sbm@sbmkorea.biz' ); 
 $mail->Priority = 1;
-
-#$mail->AddAddress ( "sbm@sbmkorea.url.ph", "SBM" );
 $mail->AddAddress ( "kpbaek@sbmkorea.com", "SBM" );
+
 
 $mail->Subject = "Message from  Contact form";
 $mail->Body = "test";
