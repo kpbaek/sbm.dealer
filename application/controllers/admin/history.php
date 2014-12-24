26e85d89ebd353c62427ba6e99de31a7d1d4e778
KPBAEK <KPBAEK@sbmkorea.com>
2014-12-24 17:42:44 +0900

    Change-Id: Id5aae08c2180fe31fc8adc3e37baea3c2bfb17b8

---
+++ application/controllers/admin/history.php
@@ -0,0 +1,55 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class History extends CI_Controller {
+
+	/**
+	 * Index Page for this controller.
+	 *
+	 * Maps to the following URL
+	 * 		http://example.com/index.php/welcome
+	 *	- or -  
+	 * 		http://example.com/index.php/welcome/index
+	 *	- or -
+	 * Since this controller is set as the default controller in 
+	 * config/routes.php, it's displayed at http://example.com/
+	 *
+	 * So any other public methods not prefixed with an underscore will
+	 * map to /index.php/welcome/<method_name>
+	 * @see http://codeigniter.com/user_guide/general/urls.html
+	 */
+	public function index()
+	{
+		$this->load->view('/admin/history/main');
+	}
+
+	function test()
+	{
+	
+		$arr = array(array('a'=>1),array('b'=>2),array('c'=>3));
+		//$arr = array('a','b','c');
+		var_dump($arr);
+		echo "<BR><BR>";
+		$d = shuffle($arr);
+		var_dump($arr);
+	
+	}
+	
+	public function body()
+	{
+		$this->load->view('/admin/history/body');
+	}
+	
+	public function tab01()
+	{
+		$this->load->view('/admin/history/tab01');
+	}
+	
+	public function listSndMail()
+	{
+		$this->load->view('/admin/history/listSndMail');
+	}
+	
+}
+
+/* End of file welcome.php */
+/* Location: ./application/controllers/welcome.php */
