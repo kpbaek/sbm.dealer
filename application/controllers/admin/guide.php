d464b65588b3bcd2f8287973674b2d5e9ba261b0
KPBAEK <KPBAEK@sbmkorea.com>
2015-01-05 17:19:14 +0900

    Change-Id: If4a90cb41d457ba2c897e248240214b268aab721

---
+++ application/controllers/admin/guide.php
@@ -0,0 +1,50 @@
+<?php if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+class Guide extends CI_Controller {
+
+	/**
+	 * Index Page for this controller.
+	 *
+	 * Maps to the following URL
+	 * 		http://example.com/index.php/welcome
+	 *	- or -  
+	 * 		http://example.com/index.php/welcome/index
+	 *	- or -
+	 * Since this controller is set as the default controller in 
+	 * config/routes.php, it's displayed at http://example.com/
+	 *
+	 * So any other public methods not prefixed with an underscore will
+	 * map to /index.php/welcome/<method_name>
+	 * @see http://codeigniter.com/user_guide/general/urls.html
+	 */
+	public function index()
+	{
+		$this->load->view('/admin/guide/main');
+	}
+
+	function test()
+	{
+	
+		$arr = array(array('a'=>1),array('b'=>2),array('c'=>3));
+		//$arr = array('a','b','c');
+		var_dump($arr);
+		echo "<BR><BR>";
+		$d = shuffle($arr);
+		var_dump($arr);
+	
+	}
+	
+	public function body()
+	{
+		$this->load->view('/admin/guide/body');
+	}
+	
+	public function tab01()
+	{
+		$this->load->view('/admin/guide/tab01');
+	}
+	
+}
+
+/* End of file welcome.php */
+/* Location: ./application/controllers/welcome.php */
