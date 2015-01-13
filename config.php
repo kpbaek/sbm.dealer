b51a3ffceaf73bdd2e89c0bda066293576b934af
KPBAEK <KPBAEK@sbmkorea.com>
2015-01-13 17:31:07 +0900

    Change-Id: I1822b2f76d009bd1dbf5ba6647e37179f7c33788

--- config.php
+++ config.php
@@ -1,18 +1,4 @@
 <?php
-// PHPGRID database connection settings
-define("PHPGRID_DBTYPE","mysql"); // or mysqli
-define("PHPGRID_DBHOST","localhost");
-define("PHPGRID_DBUSER","sbmkorea");
-define("PHPGRID_DBPASS","sbmkoreacom123");
-define("PHPGRID_DBNAME","test");
-
-// Automatically make db connection inside lib
-define("PHPGRID_AUTOCONNECT",0);
-
-// Basepath for lib
-#define("PHPGRID_LIBPATH",dirname(__FILE__).DIRECTORY_SEPARATOR."lib".DIRECTORY_SEPARATOR);
-define("PHPGRID_LIBPATH",$_SERVER["DOCUMENT_ROOT"]."/lib/");
-
 // Local database connection settings
 define("SBMBIZ_DBTYPE","mysql"); // or mysqli
 define("SBMBIZ_DBHOST","localhost");
