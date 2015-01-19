d125893084afa361ffc8924c641c56d3c68a8a0d
KPBAEK <KPBAEK@sbmkorea.com>
2015-01-19 16:32:30 +0900

    Change-Id: I79c4bf9d99455830daa2b79ea6ee5faa3022e036

---
+++ include/email/00710001.php
@@ -0,0 +1,169 @@
+<!DOCTYPE html PUBLIC "-//W3C//DTD XHTML 1.1//EN" "http://www.w3.org/TR/xhtml11/DTD/xhtml11.dtd">
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+
+<style type="text/css">
+html {
+	font-family: Calibri, Arial, Helvetica, sans-serif;
+	font-size: 11pt;
+	background-color: white
+}
+
+table {
+	border-collapse: collapse;
+	page-break-after: always;
+}
+
+.gridlines td {
+	border: 1px dotted black
+}
+
+.b {
+	text-align: center
+}
+
+.e {
+	text-align: center
+}
+
+.f {
+	text-align: right
+}
+
+.inlineStr {
+	text-align: left
+}
+
+.n {
+	text-align: right
+}
+
+.s {
+	text-align: left
+}
+
+td.style01 {
+	vertical-align: middle;
+	text-align: center;
+	padding-left: 0px;
+	border-bottom: none #000000;
+	border-top: 1px solid #000000 !important;
+	border-left: 1px solid #000000 !important;
+	border-right: none #000000;
+	font-weight: bold;
+	color: #000000;
+	font-family: 'Calibri';
+	font-size: 11pt;
+	background-color: #A0A0A0
+}
+
+table.sheet0 col.col0 {
+	width: 42pt
+}
+
+table.sheet0 col.col1 {
+	width: 42pt
+}
+
+table.sheet0 col.col2 {
+	width: 42pt
+}
+
+table.sheet0 col.col3 {
+	width: 42pt
+}
+
+table.sheet0 col.col4 {
+	width: 42pt
+}
+
+table.sheet0 col.col5 {
+	width: 42pt
+}
+
+table.sheet0 col.col6 {
+	width: 42pt
+}
+
+table.sheet0 col.col7 {
+	width: 82pt
+}
+
+table.sheet0 col.col8 {
+	width: 82pt
+}
+
+table.sheet0 tr {
+	height: 15pt
+}
+</style>
+
+
+<table border="0" cellpadding="1" cellspacing="1" id="sheet0"
+	width="950">
+	<col class="col0">
+	<col class="col1">
+	<col class="col2">
+	<col class="col3">
+	<tbody>
+		<tr class="row0">
+			<td colspan=4 class="style01">Finder Info.</td>
+		</tr>
+		<tr height="5px">
+			<td colspan=4></td>
+		</tr>
+		<tr>
+			<td width="17%" class="style01">Name</td>
+			<td width="3%"></td>
+			<td width="35%">@usr_nm</td>
+			<td>&nbsp;</td>
+		</tr>
+		<tr>
+			<td class="style01">Company</td>
+			<td class="column1">&nbsp;</td>
+			<td>@usr_cmpy_nm</td>
+			<td class="column3">&nbsp;</td>
+		</tr>
+		<tr>
+			<td class="style01">Business type</td>
+			<td width="3%"></td>
+			<td>@txt_biztp_atcd</td>
+			<td class="column3 style7 s"></td>
+		</tr>
+		<tr>
+			<td class="style01">Address</td>
+			<td class="column1">&nbsp;</td>
+			<td colspan=2>@usr_addr</td>
+		</tr>
+		<tr>
+			<td class="style01">Country</td>
+			<td width="3%"></td>
+			<td class="column2 style13 s">@txt_nation</td>
+			<td class="column3 style0 s"></td>
+		</tr>
+		<tr>
+			<td class="style01">Email address</td>
+			<td width="3%"></td>
+			<td>@usr_email</td>
+			<td class="column3">&nbsp;</td>
+		</tr>
+		<tr>
+			<td class="style01">Telephone number</td>
+			<td width="3%"></td>
+			<td>@usr_tel</td>
+			<td class="column3">&nbsp;</td>
+		</tr>
+		<tr>
+			<td class="style01">Comments</td>
+			<td class="column1">&nbsp;</td>
+			<td colspan=3>@comments</td>
+		</tr>
+		<tr class="row19">
+			<td class="column0">&nbsp;</td>
+			<td class="column1">&nbsp;</td>
+			<td class="column2 style11 null"></td>
+			<td class="column3 style12 null"></td>
+		</tr>
+	</tbody>
+</table>
