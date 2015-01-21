d0cc58408bd650d8e1900b961ac4b29aca9d98f8
KPBAEK <KPBAEK@sbmkorea.com>
2015-01-21 18:04:41 +0900

    Change-Id: I9914645ed43eb237e8acb4090add19fcc3744fef

---
+++ application/views/admin/guide/tab02.php
@@ -0,0 +1,39 @@
+<?php
+require $_SERVER ["DOCUMENT_ROOT"] . '/include/user/authAdm.php';
+?>
+<!DOCTYPE html> 
+<html>
+<head>
+<meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
+
+<style type="text/css">
+html {
+	font-family: Calibri, Arial, Helvetica, sans-serif;
+	font-size: 11pt;
+	background-color: white
+}
+
+table {
+	page-break-after: always;
+}
+</style>
+
+</head>
+
+<body>
+
+	<div id="guide_tab02" align=center>
+
+		<table border=1 cellpadding=0 cellspacing=0 width=827>
+			<tr>
+				<td align='center'><img src="/images/admin/guide/om_v1.png"></td>
+
+			</tr>
+		</table>
+	</div>
+</body>
+
+</html>
+
+
+
